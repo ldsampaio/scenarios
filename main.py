fd83e7dda33cd266d174de5a17cbd3c038de7098
Lucas <sampaio@ieee.org>
2019-04-04 06:19:05 -0300

    v0.4

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ def hexagon(cx,cy):
 
 def drawHexagon(x,y):
     for i in range(0, 6):
-        plt.plot([x[i], x[(i+1)%len(x)]], [y[i],y[(i+1)%len(y)]], 'r-', linewidth=2.0)
-    del i
+        plt.plot([x[i], x[(i+1) % len(x)]], [y[i], y[(i+1) % len(y)]], 'r-', linewidth=2.0)
     return 1
 
 
@@ -43,14 +42,13 @@ def permutations(C):
     perms = [p for p in itertools.product(range(-(C - 1), C), repeat=2)]
 
     for i in list(perms):
-        if (i[1] == 0 and i[0] % 2 != 0):
+        if i[1] == 0 and i[0] % 2 != 0:
             perms.remove(i)
-        if (i[0] % 2 != 0 and abs(i[0]) + abs(i[1]) > C+1):
+        if i[0] % 2 != 0 and abs(i[0]) + abs(i[1]) > C+1:
             perms.remove(i)
-        #if (abs(i[0]) + abs(i[1]) > C and C < 4) or abs(i[0]) + abs(i[1]) > C+1:
         if abs(i[0]) + abs(i[1]) > C and abs(i[0]) % 2 == 0:
             perms.remove(i)
-    del i
+
     return perms
 
 
@@ -58,52 +56,49 @@ def genPositions(R):
     x = np.linspace(-R, R, 2*R+1)
     y = np.linspace(int(np.ceil(-R*S60)), int(np.floor(R*S60)), int(np.floor(R*S60) - np.ceil(-R*S60) + 1))
     xg, yg = np.meshgrid(x, y, sparse=False, indexing='ij')
+    rx = np.zeros((xg.shape[0], xg.shape[1]), np.float)
+    ry = np.zeros((yg.shape[0], yg.shape[1]), np.float)
     for i in range(0, len(x)):
         for j in range(0, len(y)):
-            if xg[i,j] == 0 and yg[i,j] == 0:
-                np.delete(xg,[i, j])
-                np.delete(yg,[i, j])
-            elif isInHexagon(xg[i,j],yg[i,j],0,0,R) != 1:
-                np.delete(xg,[i, j])
-                np.delete(yg,[i, j])
-    del i, j, x, y
-    return xg, yg
+            if (xg[i, j] != 0 or yg[i, j] != 0) and isInHexagon(xg[i, j], yg[i, j], 0, 0, R):
+                rx[i, j] = float(xg[i, j])
+                ry[i, j] = float(yg[i, j])
+    del x, y, xg, yg
+    return rx, ry
 
 
 def transferPosition(x, y, cx, cy):
     return x+cx, y+cy
 
 
-def randomPosition(xg,yg):
+def randomPosition(xg, yg, kx, ky):
     i = -1
     j = -1
     while i < 0 or j < 0:
         try:
             i = np.random.randint(0, len(xg)-1, 1)
             j = np.random.randint(0, len(yg)-1, 1)
-            tempx = xg[i,j]
-            tempy = yg[i,j]
+            tempx = xg[i, j]
+            tempy = yg[i, j]
+            if tempx == 0 and tempy == 0:
+                i = -1
+            elif tempx in kx and tempy in ky:
+                i = -1
         except IndexError:
             i = -1
-            j = -1
     return tempx, tempy
 
 
-def isInHexagon(x,y,cx,cy,R):
+def isInHexagon(x, y, cx, cy, R):
     dx = abs(x-cx)
     dy = abs(y-cy)
     if dx > R or dy > R*S60:
         return False
-    #elif R * R * S60 - R * dx - R * S60 * dy >= 0:
-        #print(dx, dy, R * R * S60 - R * dx - R * S60 * dy)
-        #return False
-    elif -2 * np.sqrt(3) * dx + 2*R*np.sqrt(3) - dy < 0:
+    elif - np.sqrt(3) * dx + R * np.sqrt(3) - dy < 0:
         return False
     else:
         return True
-    # a = 0.25 * S60
-    # print(a,dx,dy)
-    # return (dy <= a) and (a*dx + 0.25 * dy <= 0.5 * a)
+
 
 
 ########################################################################################################################
@@ -111,7 +106,7 @@ def isInHexagon(x,y,cx,cy,R):
 ########################################################################################################################
 
 #Cluster Size (1 to 4 works)
-C = 1
+C = 4
 #Base Stations (1 per ce'll)
 BS = chn(C)
 #Cell Radius
@@ -146,17 +141,9 @@ for j in idx:
         x, y = hexagon(c_x[i], c_y[i])
         drawHexagon(x,y)
     for k in range(0, K):
-        # k_x[i][k] = np.random.uniform(-R + c_x[i], R + c_x[i], 1)
-        # k_y[i][k] = np.random.uniform(-R + c_y[i], R + c_y[i], 1)
-        # while isInHexagon(k_x[i][k], k_y[i][k], c_x[i], c_y[i], R) != 1:
-        #     k_x[i][k] = np.random.uniform(-R + c_x[i], R + c_x[i], 1)
-        #     k_y[i][k] = np.random.uniform(-R + c_y[i], R + c_y[i], 1)
-        k_x[i, k], k_y[i, k] = randomPosition(xg, yg)
+        k_x[i, k], k_y[i, k] = randomPosition(xg, yg, k_x, k_y)
         k_x[i, k], k_y[i, k] = transferPosition(k_x[i, k], k_y[i, k], c_x[i], c_y[i])
-        print(k_x[i, k], k_y[i, k])
-        print(-2 * np.sqrt(3) * abs(k_x[i, k]) + 2*R*np.sqrt(3) - abs(k_y[i, k]))
         drawUser(k_x[i][k], k_y[i][k])
     i += 1
 
-#print(k_x, k_y)
 plt.show()
